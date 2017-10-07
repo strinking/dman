59c7b32328884d04b1a9891ed3902dc6d09665f7
unleashy <lakeavenger@gmail.com>
2017-10-06 22:11:32 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# dman
+Pacman but in [D](https://dlang.org/).
+
+## Installation
+You'll need SDL2. Build with `dub build`.
+
+## License
+MIT.
---
+++ source/app.d
@@ -0,0 +1,10 @@
+import std.stdio;
+
+import derelict.sdl2.sdl,
+       derelict.sdl2.image;
+
+void main()
+{
+    DerelictSDL2.load();
+    DerelictSDL2Image.load();
+}
